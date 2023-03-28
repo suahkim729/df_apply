4fb2e798ad0a1a1b1005a9e6d40b3575a88e616d
Suah Kim <suahkim2729@gmail.com>
2023-03-28 19:17:15 -0400

    finished our basic function!

--- R/dfapply.R
+++ R/dfapply.R
@@ -6,32 +6,17 @@ TestData <-
           integer = (1L:n) * (1L:n),    # ^2 would return a double!! #<<
           character = LETTERS[1L:n],
           factor = factor(letters[1L:n]),
-          logical = rep(c(TRUE, FALSE), length.out = n) )
+          logical = rep(c(TRUE, FALSE), length.out = n))
 
-#TestData1 <-
-  #tibble( double = rnorm(n, 100, 10))
-
-
-#fun <- function(x, digits){
-  #if (is.numeric(x)) round(x, digits = digits) else x
- # round(x, digits = digits)
-#}
-
-#filter <- function(x){
- # if (is.numeric(x)) {return (TRUE)} else {return (FALSE)}}
-  #return (TRUE)
-#}
-
-#df_apply <- function(.data, .fun = round, .filter = is.numeric, digits){
- # if (filter(.data))
-  #  lapply(.data, `fun`, digits = digits) |>
-   # as_tibble()}
+rfun <- function(x) round(x)
+rfilter <- function(x) is.numeric(x)
 
 
-df_apply <- function(.data, .fun = round, .filter = is.numeric, digits){
+df_apply <- function(.data, .fun, .filter) {
+  modified_columns <- lapply(.data, function(x) {
+    if (.filter(x)){.fun(x)} else {x}
+  })
+  return(data.frame(modified_columns))
 }
 
-
-
-df_apply(TestData, .fun = round, .filter = is.numeric, digits = 0)
-
+df_apply(TestData, rfun, rfilter)
