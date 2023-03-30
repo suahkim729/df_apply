adb6bf683f0dd8028cb4ceb7537b7d941979e024
Suah Kim <suahkim2729@gmail.com>
2023-03-30 19:07:52 -0400

    added rd!

--- R/dfapply.R
+++ R/dfapply.R
@@ -1,4 +1,27 @@
-library(dplyr)
+
+#' Apply a function that can be applied to a columns of a data frame
+#'
+#' @param .data should expect a data frame
+#' @param .fun  expect a function that can be applied to a column of a data frame and return a column of a data frame
+#' @param .filter should expect a function that can be applied to a columns of a data frame and returns a logical (TRUE or FALSE)
+#'
+#'
+#' @return data frame that is a modified version of .data
+#' @export
+#'
+#' @examples
+#'
+#' TestData <-tibble( double = rnorm(n, 100, 10),
+#'   x = 123400 / 10^(1L:n),
+#'   integer = (1L:n) * (1L:n),
+#'   character = LETTERS[1L:n],
+#'   factor = factor(letters[1L:n]),
+#'   logical = rep(c(TRUE, FALSE), length.out = n))
+#' df_apply <- function(.data, .fun, .filter) {modified_columns <-lapply(.data, function(x) {if (.filter(x)){.fun(x)} else {x}}) return(data.frame(modified_columns))}
+#' df_apply(TestData, rfun, rfilter)
+
+
+
 n <- 7L
 TestData <-
   tibble( double = rnorm(n, 100, 10),
