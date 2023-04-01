5b67e68c4801b2eeaf7b5a1514b95609c3e244d7
Suah Kim <suahkim2729@gmail.com>
2023-03-31 21:43:38 -0400

    tomato data frame

---
+++ R/tomato-dataframe.R
@@ -0,0 +1,21 @@
+#' Collection of daily tomato prices from 2013 to 2021.
+#'
+#' A data set has 6 columns and 2742 rows.
+#'
+#' Variables:
+#'
+#' Date: the date of the price data
+#' Maximum Price: the highest price of tomatoes for that day
+#' Minimum Price: the lowest price of tomatoes for that day
+#' Average Price: the average price of tomatoes for that day
+#'
+#' This dataset can be used to analyze trends and patterns in tomato prices over time, as well as to make predictions about future tomato prices.
+#'
+#' @docType data frame
+#'
+#' @author Ram Rijal
+#'
+#' @source <https://www.kaggle.com/datasets/ramkrijal/tomato-daily-prices?resource=download>
+#' @usage data
+#'
+#'
