bb4f8f79ec4fb18bbfe366bf9a28124b6e7b2955
SheriseI <sei4@calvin.edu>
2023-04-10 18:40:48 -0400

    Added documentation for the cereal dataset.

---
+++ R/cereal-dataframe.R
@@ -0,0 +1,9 @@
+#' Cereal dataframe
+#'
+#' Variables:
+#' calories;protein;fat;sodium;fiber;carbo;sugars;potass;vitamins;shelf;weight;cups;rating
+#'
+#' @doctype dataframe
+#'
+#' @source <https://www.deseret.com/1990/5/8/18860646/top-shelve-those-sugary-cereals>
+#'
