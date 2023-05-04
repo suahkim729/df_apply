5f3bfc87710c36035f275bba74ca9c203458345f
Randall Pruim <rpruim@gmail.com>
2023-05-04 10:02:32 -0400

    remove cruft

--- R/dfapply.R
+++ R/dfapply.R
@@ -1,6 +1,8 @@
 
 #' Apply a function that can be applied to a columns of a data frame (rounding number)
 #'
+#' Apply a function that can be applied to a columns of a data frame (rounding number)
+#'
 #' @param .data should expect a data frame
 #' @param .fun  expect a function that can be applied to a column of a data frame and return a column of a data frame
 #' @param .filter should expect a function that can be applied to a columns of a data frame and returns a logical (TRUE or FALSE)
@@ -11,20 +13,12 @@
 #' @export
 #'
 #' @examples
-#'df_apply <- function(.data, .fun, .filter) {modified_columns <- lapply(.data, function(x) {
-#'if (.filter(x)){.fun(x)} else {x}})
-#'return(data.frame(modified_columns))}
-#'
-#'rfun <- function(x) round(x)
-#'rfilter <- function(x) is.numeric(x)
 #'
 #' TestData <- data.frame(A = c(1.092, 300.81, 399.203, 20.4934),
 #' B = c(503.47, 29.89, 293.90, 3.01),
 #' C = c(80.29, 19.867, 39.20, 182.93))
 #'
-#' df_apply(TestData, rfun, rfilter)
-
-
+#' df_apply(TestData, round, is.numeric)
 
 
 df_apply <- function(.data, .fun, .filter) {
@@ -34,14 +28,14 @@ df_apply <- function(.data, .fun, .filter) {
   return(data.frame(modified_columns))
 }
 
-rfun <- function(x) round(x)
-rfilter <- function(x) is.numeric(x)
-
-
-df_apply(TestData, rfun, rfilter)
-
-TestData <- data.frame(A = c(1.092, 300.81, 399.203, 20.4934),
-                       B = c(503.47, 29.89, 293.90, 3.01),
-                       C = c(80.29, 19.867, 39.20, 182.93))
-
-
+# rfun <- function(x) round(x)
+# rfilter <- function(x) is.numeric(x)
+#
+#
+# df_apply(TestData, rfun, rfilter)
+#
+# TestData <- data.frame(A = c(1.092, 300.81, 399.203, 20.4934),
+#                        B = c(503.47, 29.89, 293.90, 3.01),
+#                        C = c(80.29, 19.867, 39.20, 182.93))
+#
+#
